f4a7041e3552474037dcd79092d3d74ff95bde55
kedarmayekar077 <kedarmayekar077@gmail.com>
2024-02-21 17:14:51 +0530

    Added hero image and styling

---
+++ src/HeroSection.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HeroSection.css";
+
+const HeroSection = () => {
+    return (
+        <div className="hero-section">
+            <div className="hero-content">
+                <h1>Google cloud Image Uploader</h1>
+                <p>You can upload your images to google cloud from here.</p>
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ImageUploader from './ImageUploader';
-import CloudImageUploader from './CloudImageUploader';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <ImageUploader />
-    <CloudImageUploader/>
   </React.StrictMode>
 );
 
